57ae890f23ce16051135b28811f83d7341440cc8
fullkon88 <fullkon999@gmail.com>
2023-12-13 14:37:23 +0300

    Domashka

---
+++ Desktop/C#_work/2_urok_4_zadanie/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 4: Напишите программу, которая на вход принимает натуральное число N, а на выходе показывает его цифры через запятую.
+
+System.Console.WriteLine("Введите натуральное число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+if(N < 10)
+{
+    System.Console.WriteLine(N);
+}
+
+else
+{
+    while( N > 0)
+    {
+        int current = N % 10;
+        N /= 10;
+        if ( N > 0)
+        {
+            System.Console.Write(current + ",");
+        }
+        else
+        {
+            System.Console.Write(current);
+        }
+    }
+}
